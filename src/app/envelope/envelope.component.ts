1be8a16ffe486709eaebd976291fccb9e865ae87
Brandon <brandonbannavong2355@gmail.com>
2024-03-24 18:49:49 -0400

    update beat

---
+++ src/app/envelope/envelope.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-envelope',
+  standalone: true,
+  imports: [],
+  templateUrl: './envelope.component.html',
+  styleUrl: './envelope.component.css'
+})
+export class EnvelopeComponent {
+
+}
